d31fa3a37c4a8690de52b22c81672aab1b8caa54
Tsalsabilla <tsalsabilla569@gmail.com>
2024-04-27 08:29:01 +0700

    alibaba

--- src/BackendURL.js
+++ src/BackendURL.js
@@ -1,5 +1,5 @@
 // const BackendURL = "https://lms-studee-server-production.up.railway.app"
-// const BackendURL = "http://127.0.0.1:4500"
-const BackendURL = "http://localhost:4500"
+const BackendURL = "http://47.236.0.158/api"
+// const BackendURL = "http://localhost:4500"
 
 export default BackendURL;
