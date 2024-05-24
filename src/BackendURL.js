a00fde99fbee4935640a929369e19270acd8b4aa
DCahyono <afeoriginator2@gmail.com>
2024-05-24 20:00:20 +0700

    vercel

--- src/BackendURL.js
+++ src/BackendURL.js
@@ -1,4 +1,5 @@
-const BackendURL = "https://lms-studee-server-production.up.railway.app"
+// const BackendURL = "https://lms-studee-server-production.up.railway.app"
+const BackendURL = "https://lms-studee-server.vercel.app"
 // const BackendURL = "http://47.236.0.158/"
 // const BackendURL = "http://localhost:4500"
 
